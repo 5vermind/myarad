450333bf4f6df26b02cd24a58d15e33d9e91d064
kim taehyun <5vermind@gmail.com>
2022-10-10 21:07:17 +0900

    seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import { Container } from "@nextui-org/react"
 import type { NextPage } from "next"
+import Head from "next/head"
 import { LandingPage } from "src/components/landing/LandingPage"
 
 const Home: NextPage = () => (
   <Container>
+    <Head>
+      <title>던파마부</title>
+      <meta name="description" content="던파 마부를 한눈에! " />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta property="og:title" content="던파마부" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://dfmabu.vercel.app" />
+      <meta property="og:article:author" content="던파마부" />
+    </Head>
     <LandingPage />
   </Container>
 )
