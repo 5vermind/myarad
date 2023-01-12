94991077907c7bca7dd61dbdcc221e2aa1c72d03
5vermind <5vermind@gmail.com>
2023-01-12 17:30:06 +0900

    init

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,22 @@
-import * as React from "react";
-export const Button = () => {
-  return <button>Boop</button>;
+interface ButtonProps {
+  primary?: boolean;
+  size?: "small" | "medium" | "large";
+  label?: string;
+}
+
+export const Button = ({
+  primary = false,
+  size = "medium",
+  label = "button",
+}: ButtonProps) => {
+  return (
+    <button
+      style={{
+        backgroundColor: primary ? "red" : "blue",
+        fontSize: size === "small" ? 12 : size === "medium" ? 16 : 20,
+      }}
+    >
+      {label}
+    </button>
+  );
 };
